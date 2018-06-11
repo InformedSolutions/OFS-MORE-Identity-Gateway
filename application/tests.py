73b0521f60540dd505c9bd348b08915b12807d79
thomas-95is <thomas.stokes@informed.com>
2018-06-11 15:22:18 +0100

    tests.py: Create a test case for Jenkins build to find.

--- application/tests.py
+++ application/tests.py
@@ -1,3 +1,15 @@
+import os
+
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+
+class IdentityGatewayTests(TestCase):
+    def test_get_request_returns_404_for_nonexistent_email(self):
+        """
+        Test that making a query for an email that is not associated with an account returns a 404 response code.
+        """
+        prefix = settings.PUBLIC_APPLICATION_URL
+        response = self.client.get(prefix + '/api/v1/user/?email=nonexistentemail%40test.com')
+
+        self.assertEqual(response.status_code, 404)
