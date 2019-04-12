fba076f7d72cf2d116d307adf4c486d89dc67b55
jenniferbrown94 <jennifer.brown@informed.com>
2019-04-12 11:52:05 +0100

    updating serializer

--- application/models/user_details.py
+++ application/models/user_details.py
@@ -65,7 +65,7 @@ class UserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserDetails
         fields = ('login_id', 'email', 'mobile_number', 'add_phone_number', 'email_expiry_date', 'sms_expiry_date',
-                  'magic_link_email', 'magic_link_sms', 'sms_resend_attempts', 'sms_resend_attempts_expiry_date', 'application_id', 'change_email')
+                  'magic_link_email', 'magic_link_sms', 'sms_resend_attempts', 'sms_resend_attempts_expiry_date', 'application_id', 'change_email', 'service')
 
     def get_summary_table(self):
         data = self.data
